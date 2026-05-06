12b20c16a59f7763b8bf9ce874a8da4eb156ba1f
priscillatra <bluuhsaes@example.com>
2026-05-06 02:38:23 +0000

    Use hardcoded cloudbuild.yaml with dummy test

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
+from flask import Flask
 import os
-import random
-from flask import Flask, jsonify
 
 app = Flask(__name__)
-secret_number = random.randint(1, 100)
+SECRET_NUMBER = 42
 
 @app.route('/guess/<int:guess>')
 def guess(guess):
-    global secret_number
-    if guess < secret_number:
-        return jsonify({"msg": "Higher"})
-    elif guess > secret_number:
-        return jsonify({"msg": "Lower"})
+    if guess < SECRET_NUMBER:
+        return {"msg": "Higher"}
+    elif guess > SECRET_NUMBER:
+        return {"msg": "Lower"}
     else:
-        old = secret_number
-        secret_number = random.randint(1, 100)
-        return jsonify({"msg": "Correct! New game started.", "old_secret": old})
-
-@app.route('/')
-def home():
-    return jsonify({"msg": "Guess a number between 1 and 100 at /guess/<number>"})
+        return {"msg": "Correct! You win!"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
